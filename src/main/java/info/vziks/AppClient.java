3a5ab8548aeb85067a87385bc6ccdf21f01ef996
vziks <vziks@live.ru>
2021-03-11 14:37:46 +0300

    +

--- src/main/java/info/vziks/AppClient.java
+++ src/main/java/info/vziks/AppClient.java
@@ -1,7 +1,37 @@
-package info.vziks;/**
+package info.vziks;
+
+import info.vziks.grpc.AppServiceGrpc;
+import info.vziks.grpc.AppServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+/**
  * Class AppClient
  * Project grpc-demo-client
  *
  * @author Anton Prokhorov <vziks@live.ru>
- */public class AppClient {
+ */
+public class AppClient {
+
+    public static void main(String[] args) {
+
+        String[] arrNames = new String[]{
+                "Liam",
+                "Charlotte",
+                "Ava",
+                "Ethan",
+                "Mason",
+                "Amelia"
+        };
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080").usePlaintext().build();
+
+        for (String name : arrNames) {
+            AppServiceGrpc.AppServiceBlockingStub stub = AppServiceGrpc.newBlockingStub(channel);
+            AppServiceOuterClass.AppRequest request = AppServiceOuterClass.AppRequest.newBuilder().setName(name).build();
+            AppServiceOuterClass.AppResponse response = stub.hello(request);
+            System.out.println(response);
+        }
+
+        channel.shutdownNow();
+    }
 }
